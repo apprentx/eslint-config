328c2f2f6d595c88950abda368536a11710e6274
Alex Nault <nault.alex@gmail.com>
2024-11-01 14:27:29 -0400

    config: Forbid ts-ignore (#2)
    * config: Forbid ts-ignore
    
    * WIP
    
    * WIP

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ module.exports = {
     "no-use-before-define": "off", // uses "@typescript-eslint/no-use-before-define" instead
     "max-params": ["error", 4],
     yoda: ["error", "never"],
-    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
+      {
+        "ts-check": true,
+        "ts-expect-error": false,
+        "ts-ignore": true,
+        "ts-nocheck": true,
+      },
+    ],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { ignoreRestSiblings: true }],
   },
